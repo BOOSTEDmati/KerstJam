7d98a593658448202334206f1ca167c5f466791d
BOOSTEDmati <25820@ma-web.nl>
2020-12-18 13:19:47 +0100

    GameJAM

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    private bool IJ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IJ = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)&& IJ == false)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 19, 0);
+
+            IJ = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            IJ = false;
+        }
+
+        if (collision.gameObject.tag == "Respawn")
+        {
+            SceneManager.LoadScene("SampleScene");
+            ScoreScript.scoreValue = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    private GameObject Spawn;
+    [SerializeField]
+    private GameObject Tree;
+
+    private float SpawnTime = 2.5f;
+
+    void Start()
+    {
+
+        StartCoroutine(spawn());
+    }
+
+
+    void A()
+    {
+        Spawn = Instantiate(Tree, transform.position, Quaternion.identity) as GameObject;
+        ScoreScript.scoreValue += 1;
+    }
+
+    IEnumerator spawn() 
+{
+    while(true)
+    {
+        yield return new WaitForSeconds(Random.Range(1f, SpawnTime));
+
+        A();
+    }
+}
+}
